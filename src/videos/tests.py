cbdf48727c3a1a9f230e35960c1d2ce4ceba7d72
Fedi Nabli <fedinabli@gmail.com>
2023-01-08 09:28:45 +0100

    Added tests for publish state options

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-
+from django.utils import timezone
 from .models import Video
 
 class VideoModelTestCase(TestCase):
   def setUp(self):
     Video.objects.create(title='This is my title')
+    Video.objects.create(title='This is my title', state=Video.VideoStateOptions.PUBLISH)
 
   def test_valid_title(self):
     title = 'This is my title'
@@ -13,4 +14,16 @@ class VideoModelTestCase(TestCase):
 
   def test_created_count(self):
     qs = Video.objects.all()
+    self.assertEqual(qs.count(), 2)
+
+  def test_draft_case(self):
+    qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
     self.assertEqual(qs.count(), 1)
+
+  def test_publish_case(self):
+    qs = Video.objects.filter(state=Video.VideoStateOptions.PUBLISH)
+    published_qs = Video.objects.filter(
+      state=Video.VideoStateOptions.PUBLISH,
+      publish_timestamp__lte=timezone.now()
+    )
+    self.assertTrue(published_qs.exists())
