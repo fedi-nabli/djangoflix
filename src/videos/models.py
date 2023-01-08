bbb9d5ff26815bd370010f4d9e1a2dc91ee6b98c
Fedi Nabli <fedinabli@gmail.com>
2023-01-08 09:19:53 +0100

    Added CharField choices for publishing videos

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,16 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Video(models.Model):
+  class VideoStateOptions(models.TextChoices):
+    # CONSTANT = DB_VALUE, USER_DISPLAY_VALUE
+    PUBLISH = 'PU', 'Publish'
+    DRAFT = 'DR', 'Draft'
+    # UNLISTED = 'UN', 'Unslisted'
+    # PRIVATE = 'PR', 'Private'
+
   title = models.CharField(max_length=220)
   description = models.TextField(blank=True, null=True)
   slug = models.SlugField(blank=True, null=True)
   video_id = models.CharField(max_length=220)
   active = models.BooleanField(default=True)
+  state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+  publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
   @property
   def is_published(self):
     return self.active
+
+  def save(self, *args, **kwargs):
+    if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+      print("save as published")
+      self.publish_timestamp = timezone.now()
+    elif self.state == self.VideoStateOptions.DRAFT:
+      self.publish_timestamp = None
+    super().save(*args, **kwargs)
 
 class VideoAllProxy(Video):
   class Meta:
